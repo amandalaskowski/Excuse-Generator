56f6744591356a3704fba19eb7d7b25c34d61a01
amandalaskowski <amanda.2002@aol.com>
2021-05-28 20:56:03 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let first = ["My dog", "My mom", "The cat", "My neighbor"];
+  let second = ["ate my", "took my", "threw my", "destroyed my"];
+  let third = ["homework", "shoes", "lunch", "dinner"];
+  let fourth = ["last night.", "this morning.", "at work.", "after school."];
+
+  let firstIndex = 0;
+  let secondIndex = 0;
+  let thirdIndex = 0;
+  let fourthIndex = 0;
+
+  return (
+    first[firstIndex] +
+    " " +
+    second[secondIndex] +
+    " " +
+    third[thirdIndex] +
+    " " +
+    fourth[fourthIndex]
+  );
 };

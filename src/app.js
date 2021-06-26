0de69fd3bc8e4394117e8d5816417c10b4b5fb13
amandalaskowski <amanda.2002@aol.com>
2021-06-26 02:58:33 +0000

    newest

--- src/app.js
+++ src/app.js
@@ -2,29 +2,27 @@
 
 import "./style.css";
 
-window.onload = () => {
-  document.querySelector("#excuse").innerHTML = generateExcuse();
-  console.log("Hello Rigo from the console!");
-};
-
-let generateExcuse = () => {
+window.onload = function generateExcuse() {
   let first = ["My dog", "My mom", "The cat", "My neighbor"];
   let second = ["ate my", "took my", "threw my", "destroyed my"];
   let third = ["homework", "shoes", "lunch", "dinner"];
   let fourth = ["last night.", "this morning.", "at work.", "after school."];
 
-  let firstIndex = 0;
-  let secondIndex = 0;
-  let thirdIndex = 0;
-  let fourthIndex = 0;
+  let firstIndex = Math.floor(Math.random() * first.length);
+  let secondIndex = Math.floor(Math.random() * second.length);
+  let thirdIndex = Math.floor(Math.random() * third.length);
+  let fourthIndex = Math.floor(Math.random() * fourth.length);
 
-  return (
+  let generateExcuse =
     first[firstIndex] +
     " " +
     second[secondIndex] +
     " " +
     third[thirdIndex] +
-    " " +
-    fourth[fourthIndex]
-  );
+    "" +
+    fourth[fourthIndex];
+
+  document.getElementById("excuse").innerHTML = generateExcuse();
+
+  return generateExcuse();
 };

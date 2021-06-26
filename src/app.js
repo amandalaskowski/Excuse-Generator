17051959f5a9e005b4e7028c7b55b4334caa5962
amandalaskowski <amanda.2002@aol.com>
2021-06-26 15:52:53 +0000

    final

--- src/app.js
+++ src/app.js
@@ -2,27 +2,28 @@
 
 import "./style.css";
 
-window.onload = function generateExcuse() {
-  let first = ["My dog", "My mom", "The cat", "My neighbor"];
-  let second = ["ate my", "took my", "threw my", "destroyed my"];
-  let third = ["homework", "shoes", "lunch", "dinner"];
-  let fourth = ["last night.", "this morning.", "at work.", "after school."];
+window.onload = function() {
+  function generateExcuse() {
+    let first = ["My dog", "My mom", "The cat", "My neighbor"];
+    let second = ["ate my", "took my", "threw my", "destroyed my"];
+    let third = ["homework", "shoes", "lunch", "dinner"];
+    let fourth = ["last night.", "this morning.", "at work.", "after school."];
 
-  let firstIndex = Math.floor(Math.random() * first.length);
-  let secondIndex = Math.floor(Math.random() * second.length);
-  let thirdIndex = Math.floor(Math.random() * third.length);
-  let fourthIndex = Math.floor(Math.random() * fourth.length);
+    let firstIndex = Math.floor(Math.random() * first.length);
+    let secondIndex = Math.floor(Math.random() * second.length);
+    let thirdIndex = Math.floor(Math.random() * third.length);
+    let fourthIndex = Math.floor(Math.random() * fourth.length);
 
-  let generateExcuse =
-    first[firstIndex] +
-    " " +
-    second[secondIndex] +
-    " " +
-    third[thirdIndex] +
-    "" +
-    fourth[fourthIndex];
+    let excuse =
+      first[firstIndex] +
+      " " +
+      second[secondIndex] +
+      " " +
+      third[thirdIndex] +
+      "" +
+      fourth[fourthIndex];
+    return excuse;
+  }
 
   document.getElementById("excuse").innerHTML = generateExcuse();
-
-  return generateExcuse();
 };
